0956d9dfcd0325e477858d8e5c4001fba27d7392
hellen <hellendongyun@gmail.com>
2024-10-28 21:31:21 +1300

    condational render login and logout

--- components/navigation/nav.tsx
+++ components/navigation/nav.tsx
@@ -1,19 +1,33 @@
 import { auth } from "@/server/auth";
 import UserButton from "./user-button";
+import { Button } from "../ui/button";
+import Link from "next/link";
+import { LogIn } from "lucide-react";
 
 export default async function Nav() {
   const session = await auth();
   return (
     <header className="bg-slate-500 text-white py-4">
       <nav>
-        <ul className="flex justify-between items-center">
+        <ul className="flex justify-between">
           <li>logo</li>
-          <li>
-            <UserButton
-              expires={session?.expires ?? "default"}
-              user={session?.user}
-            />
-          </li>
+          {!session ? (
+            <li>
+              <Button asChild>
+                <Link className="flex gap-2" href="api/auth/signin">
+                  <LogIn size={16} />
+                  <span>Log in</span>
+                </Link>
+              </Button>
+            </li>
+          ) : (
+            <li>
+              <UserButton
+                expires={session?.expires ?? "user"}
+                user={session?.user}
+              />
+            </li>
+          )}
         </ul>
       </nav>
     </header>
--- components/navigation/user-button.tsx
+++ components/navigation/user-button.tsx
@@ -1,7 +1,13 @@
 "use client";
 
+import { signOut } from "@/server/auth";
 import { Session } from "next-auth";
 
 export default function UserButton({ user }: Session) {
-  return <div>{user?.name}</div>;
+  return (
+    <div>
+      <h1>{user?.name}</h1>
+      <button onClick={() => signOut()}>sign out</button>
+    </div>
+  );
 }
