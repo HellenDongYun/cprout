0408b332417c5c7aeeb28347de7a13e3fbede455
hellen <hellendongyun@gmail.com>
2024-10-28 18:50:35 +1300

    auth set up google and github

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
-export default function Home() {
-  return <div>this is the home page.</div>;
+export default async function Home() {
+  return (
+    <div className="text-custom min-h-50vh">
+      <h1>hey</h1>
+      <Button variant="outline">Button</Button>
+    </div>
+  );
 }
