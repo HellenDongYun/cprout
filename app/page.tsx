39ec60cf5a5f2e0e49e86801889b310b2b1abc0d
HellenDongYun <hellendongyun@gmail.com>
2024-11-11 20:09:33 +1300

    product component render the product out show in the page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
+import Products from "@/components/products/products";
 import { Button } from "@/components/ui/button";
-
+import { db } from "@/server";
+import { productVariants } from "@/server/schema";
 export default async function Home() {
+  const data = await db.query.productVariants.findMany({
+    with: {
+      variantImages: true,
+      variantTags: true,
+      product: true,
+    },
+    orderBy: (productVariants, { desc }) => [desc(productVariants.id)],
+  });
   return (
-    <div>
-      <h1>hey</h1>
-      <Button variant="outline">Button</Button>
-    </div>
+    <main>
+      <Products variants={data} />
+    </main>
   );
 }
